e4ed3657313eed9082b8844585f2144326bb5d8f
Pranav Vashi <neobuddy89@gmail.com>
2017-07-19 23:46:45 +0530

    [SQUASH] base: Allow using 4G icon instead of LTE
    [jhonboy121]: adapt for A13
    
    Change-Id: Ieb912c64a60ddd42fe7b21d18dadea293ff159c5
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    2nd commit:
    
    SystemUI: improve 4G icon switch code
    
    * adapted to A12 L and A13
    * added setting key to backup array
    * use a single settings observer and use existing mReceiverHandler instead of creating new one's
    * keep the settings observer class private
    * don't update configuration when registering observer
    * only notify listeners if necessary
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    3rd commit:
    
    SystemUI: MobileSignalController: register 4G icon settings observer for all users
    
    * update settings on user switch as well
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    [spezi77] Add a switch to toggle the 4G icon from status bar tuner
    Co-authored-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6095,6 +6095,14 @@ public final class Settings {
         public static final String LOCK_GESTURE_STATUS = "lock_gesture_status";
 
         /**
+
+        /**
+         * Whether to display 4G icon instead LTE
+         * @hide
+         */
+        public static final String SHOW_FOURG_ICON = "show_fourg_icon";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *

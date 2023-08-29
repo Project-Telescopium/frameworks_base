chaldeaprjkt/chaldea_frameworks_base

GameSpaceManager: restrict broadcast to MANAGE_GAME_MODE holders only

packages/SystemUI/src/com/android/systemui/statusbar/policy/GameSpaceManager.kt
@@ -103,7 +103,8 @@ class GameSpaceManager @Inject constructor(
             addFlags(Intent.FLAG_RECEIVER_REPLACE_PENDING
                 or Intent.FLAG_RECEIVER_FOREGROUND
                 or Intent.FLAG_RECEIVER_INCLUDE_BACKGROUND)
-            context.sendBroadcastAsUser(this, UserHandle.SYSTEM)
+            context.sendBroadcastAsUser(this, UserHandle.SYSTEM,
+                android.Manifest.permission.MANAGE_GAME_MODE)
         }
     }
 

Project-Telescopium/frameworks_base

packages/SystemUI/src/com/android/systemui/statusbar/policy/GameSpaceManager.kt
@@ -104,7 +104,8 @@ class GameSpaceManager @Inject constructor(
             addFlags(Intent.FLAG_RECEIVER_REPLACE_PENDING
                 or Intent.FLAG_RECEIVER_FOREGROUND
                 or Intent.FLAG_RECEIVER_INCLUDE_BACKGROUND)
-            context.sendBroadcastAsUser(this, UserHandle.SYSTEM)
+            context.sendBroadcastAsUser(this, UserHandle.CURRENT,
+                android.Manifest.permission.MANAGE_GAME_MODE)
         }
     }
 